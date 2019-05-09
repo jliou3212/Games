73da238a9c08c245a3c2e544297e21d14c620a84
jliou3212 <jliou3212@gmail.com>
2019-05-09 16:02:38 -0400

    wordsearch progress

--- Generators/Wordsearch.py
+++ Generators/Wordsearch.py
@@ -1,14 +1,14 @@
 import random
 
 
+# generates a 2d array filled with 0's
 def empty_board(size):
-    # generates a 2d array filled with 0's
 
     return [[0] * size for i in range(size)]
 
 
+# generates list of random words from list of 10000~ english words
 def random_words(amt):
-    # generates list of random words from list of 10000~ english words
     file_name = "english_words_common.txt"
     file = open(file_name, 'r')
     old_words = [line.rstrip() for line in file]
@@ -19,12 +19,40 @@ def random_words(amt):
     return new_words
 
 
+# inserts one word into the board
+def insert_word(b1, w1):
+    # selects starting position for word
+    startx = random.randrange(0, len(b1))
+    starty = random.randrange(0, len(b1))
+    b1[startx][starty] = w1[0]
+
+# TODO determine list of illegal movements through conditionals
+    while True:
+        if not len(w1) + startx > len(b1):
+            if not startx - len(w1) < 0:
+        if not len(w1) + starty > len(b1):
+            if not starty - len(w1) < 0:
+
+    randx = random.randrange(-1, 2)
+    randy = random.randrange(-1, 2)
+    return b1
+
+
+# fills board with list of words
 def fill_board(b1, l1):
-    # fills board with list of words
     pass
+
+
+# prints board in 2d array
+def display_board(b1):
+    for line in b1:
+        print(line)
+
 
 if __name__ == '__main__':
     board = empty_board(5)
     words = random_words(7)
-    print(words)
+    fill_board(board, words)
+    board = insert_word(board, "test")
+    display_board(board)
 
